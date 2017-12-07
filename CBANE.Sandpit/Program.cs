69b82b284207d0504b9acde6687b82250e617e06
lynchiem <matt@matthewlynch.net>
2017-12-07 14:25:02 +1000

    Fix for Linux/OSX compatibility in CBANE.Sandpit.

--- CBANE.Sandpit/Program.cs
+++ CBANE.Sandpit/Program.cs
@@ -53,7 +53,7 @@ namespace CBANE.Sandpit
             {
                 var keyInfo = Console.ReadKey(true);
 
-                if(outputStatus == ProgramStatus.IDLE && keyInfo.Key == ConsoleKey.R)
+                if(outputStatus == ProgramStatus.IDLE && keyInfo.Key == ConsoleKey.T)
                 {
                     outputStatus = ProgramStatus.TRAINING;
                     trainingTask = Task.Run(() => PerformTrainingRun());
@@ -91,123 +91,63 @@ namespace CBANE.Sandpit
 
         static void ManageDisplay()
         {
-            ResetDisplay();
+            var previousStatus = ProgramStatus.UNKNOWN;
+            var lastAutoUpdate = DateTime.Now.AddHours(-1);
 
-            var currentStatus = ProgramStatus.UNKNOWN;
-            var currentProgress = 0;
+            ulong lastTrainingCycle = 0;
 
             while(running)
             {
-                if(Console.WindowTop != 0)
-                    ResetDisplay();
+                var statusMessage = outputStatus.ToString().PadRight(8, '.');
 
-                var statusMessage = " " + outputStatus.ToString().ToUpper();
+                var now = DateTime.Now;
 
-                // Update Status Indicator
-                var statusFGColour = ConsoleColor.Black;
-                var statusBGColour = ConsoleColor.Gray;
-
-                switch(outputStatus)
+                if(outputStatus == ProgramStatus.TRAINING && previousStatus != ProgramStatus.TRAINING)
                 {
-                    case ProgramStatus.TRAINING:
-                        statusFGColour = ConsoleColor.Black;
-                        statusBGColour = ConsoleColor.Cyan;
-                        break;
-
-                    case ProgramStatus.TESTING:
-                        statusFGColour = ConsoleColor.Black;
-                        statusBGColour = ConsoleColor.Green;
-                        break;
-
-                    case ProgramStatus.EXITING:
-                        statusFGColour = ConsoleColor.Black;
-                        statusBGColour = ConsoleColor.Yellow;
-                        break;
-
-                    case ProgramStatus.IDLE:
-                    default:
-                        statusFGColour = ConsoleColor.Black;
-                        statusBGColour = ConsoleColor.Gray;
-                        break;
+                    Console.WriteLine($"[{statusMessage}] Starting 500 cycle training run...");
+                    Console.WriteLine($"[{statusMessage}] Press [X] to exit.");
                 }
 
-                if(currentStatus != outputStatus)
+                if(outputStatus == ProgramStatus.TRAINING && (now - lastAutoUpdate).TotalMilliseconds > 1000 && Trainer.Cycle > lastTrainingCycle)
                 {
-                    currentStatus = outputStatus;
-                    currentProgress = 0;
+                    string trainingScore = (Trainer.BestTrainingScore == double.MinValue) ? "N/A" : $"{Trainer.BestTrainingScore / Trainer.MaxTrainingScore * 100:0.000000}%";
+                    string trainingScoreMessage = $"[{statusMessage}] Best Score: {trainingScore}, Cycles: {Trainer.Cycle}";
+
+                    Console.WriteLine(trainingScoreMessage);
+
+                    lastAutoUpdate = DateTime.Now;
+                    lastTrainingCycle = Trainer.Cycle;
                 }
 
-                WriteAt(statusMessage.PadRight(Console.WindowWidth), 1, 0, statusFGColour, statusBGColour);
-
-                if(currentStatus != ProgramStatus.IDLE)
+                if(outputStatus == ProgramStatus.TESTING && previousStatus != ProgramStatus.TESTING)
                 {
-                    var progressLeft = statusMessage.Length;
-
-                    WriteAt((currentProgress > 0) ? "." : " ", 1, progressLeft, statusFGColour, statusBGColour);
-                    WriteAt((currentProgress > 1) ? "." : " ", 1, progressLeft + 1, statusFGColour, statusBGColour);
-                    WriteAt((currentProgress > 2) ? "." : " ", 1, progressLeft + 2, statusFGColour, statusBGColour);
-
-                    currentProgress = (currentProgress + 1 > 3) ? 0 : currentProgress + 1;
+                    Console.WriteLine($"[{statusMessage}] Testing networks...");
+                    Console.WriteLine($"[{statusMessage}] Press [X] to exit.");
                 }
 
-                // Update Scores
-                WriteAt(" BEST SCORES:".PadRight(Console.WindowWidth), 3, 0);
-
-                string trainingScore = (Trainer.BestTrainingScore == double.MinValue) ? "N/A" : $"{Trainer.BestTrainingScore / Trainer.MaxTrainingScore * 100:0.000000}%";
-                string trainingScoreMessage = $" > TRAINING: {trainingScore}";
-
-                string testingScore = (Trainer.BestTestingScore == double.MinValue) ? "N/A" : $"{Trainer.BestTestingScore / Trainer.MaxTestingScore * 100:0.000000}%";
-                string testingScoreMessage =  $" > TESTING:  {testingScore}";
-
-                WriteAt(trainingScoreMessage.PadRight(Console.WindowWidth), 4, 0);
-                WriteAt(testingScoreMessage.PadRight(Console.WindowWidth), 5, 0);
-
-                // Update Stats
-                WriteAt(" STATISTICS:".PadRight(Console.WindowWidth), 7, 0);
-
-                string cyclesMessage =   $" > CYCLES:   {Trainer.Cycle}";
-                string clustersMessage = $" > CLUSTERS: {Trainer.Supercluster.Clusters.Count}";
-
-                WriteAt(cyclesMessage.PadRight(Console.WindowWidth), 8, 0);
-                WriteAt(clustersMessage.PadRight(Console.WindowWidth), 9, 0);
-
-                // Display Menu
-                WriteAt(" OPTIONS:".PadRight(Console.WindowWidth), 11, 0);
-
-                if(currentStatus == ProgramStatus.IDLE)
+                if(outputStatus != ProgramStatus.TESTING && previousStatus == ProgramStatus.TESTING)
                 {
-                    WriteAt(" > [R] Run Training (500 cycles)".PadRight(Console.WindowWidth), 12, 0);
-                    WriteAt(" > [X] Exit".PadRight(Console.WindowWidth), 13, 0);
-                }
-                else
-                {
-                    WriteAt(" > [X] Exit".PadRight(Console.WindowWidth), 12, 0);
-                    WriteAt("".PadRight(Console.WindowWidth), 13, 0);
+                    string testingMessage = ProgramStatus.TESTING.ToString().PadRight(8, '.');
+
+                    string testingScore = (Trainer.BestTestingScore == double.MinValue) ? "N/A" : $"{Trainer.BestTestingScore / Trainer.MaxTestingScore * 100:0.000000}%";
+                    string testingScoreMessage = $"[{testingMessage}] Best Score: {testingScore}, Cycles: {Trainer.Cycle}";
+
+                    Console.WriteLine(testingScoreMessage);
                 }
 
+                if(outputStatus == ProgramStatus.IDLE && previousStatus != ProgramStatus.IDLE)
+                {
+                    Console.WriteLine($"[{statusMessage}] Press [T] to train. Press [X] to exit.");
+                }
+
+                if(outputStatus == ProgramStatus.EXITING && previousStatus != ProgramStatus.EXITING)
+                {
+                    Console.WriteLine($"[{statusMessage}] Waiting on background tasks to stop...");
+                }
+
+                previousStatus = outputStatus;
                 Thread.Sleep(250);
             }
-        }
-
-        static void ResetDisplay()
-        {
-            Console.Clear();
-            Console.CursorVisible = false;
-
-            string title = " CBANE Sandpit";
-
-            WriteAt(title.PadRight(Console.WindowWidth), 0, 0, ConsoleColor.White, ConsoleColor.DarkRed);
-        }
-
-        static void WriteAt(string message, int top, int left, ConsoleColor fgColour = ConsoleColor.White, ConsoleColor bgColour = ConsoleColor.Black)
-        {
-            Console.CursorTop = top;
-            Console.CursorLeft = left;
-
-            Console.ForegroundColor = fgColour;
-            Console.BackgroundColor = bgColour;
-
-            Console.Write(message);
         }
 
     }
